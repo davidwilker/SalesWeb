8a5c74aaa355963c8119b89ee43182b8fa923535
Davidwilker <davidwilker2510@gmail.com>
2023-05-28 21:43:29 -0300

    Criação do controlador Sales e o HTML para acesso a pag

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Services;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+
+        private readonly SellerServices _sellerServices;
+
+        public SellersController(SellerServices sellerServices)
+        {
+            _sellerServices = sellerServices;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _sellerServices.FindAll();
+            return View(list);
+        }
+    }
+}
